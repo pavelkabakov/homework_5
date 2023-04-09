a867a244f7c39b839d6c16dd29928ecc4909c060
Павел Кабаков <pavelkabakov@gmail.com>
2023-04-10 00:44:16 +0300

    задача 36 решена

--- task_36/Program.cs
+++ task_36/Program.cs
@@ -3,3 +3,44 @@
 // [3, 7, 23, 12] -> 19
 
 // [-4, -6, 89, 6] -> 0
+
+int number = 8; // задаем длинну массива
+int[] array = new int[number];
+
+FillArrayRandom(number);
+PrintArray(array);
+Console.WriteLine($"Сумма чисел на нечетных позициях = {OddIndexCalc(array)}");
+
+int[] FillArrayRandom(int length) // Метод заполнения массива
+{
+    Random random = new Random();
+
+    for (int i = 0; i < number; i++)
+    {
+        int randomValue = random.Next(-100, 100);
+        array[i] = randomValue;
+    }
+    return array;
+}
+
+void PrintArray(int[] array) // вывод массива
+{
+    for (int i = 0; i < number; i++)
+    {
+        Console.Write($"элемент {i} - ");
+        Console.WriteLine(array[i]);
+    }
+}
+
+int OddIndexCalc(int[] array) // подсчет суммы чисел на нечетных позициях
+{
+    int summ = 0;
+        for (int i = 0; i < number; i++)
+    {
+        if (i%2 != 0)
+        {
+            summ += array[i];
+        }
+    }
+    return summ;
+}
--- task_38/Program.cs
+++ task_38/Program.cs
@@ -1,5 +1,46 @@
-﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 
-// [3, 7, 23, 12] -> 19
+// [3 7 22 2 78] -> 76
 
-// [-4, -6, 89, 6] -> 0
+int number = 8; // задаем длинну массива
+double[] array = new double[number];
+
+FillArrayRandom(number);
+PrintArray(array);
+// Console.WriteLine($"Сумма чисел на нечетных позициях = {OddIndexCalc(array)}");
+
+double[] FillArrayRandom(int length) // Метод заполнения массива
+{
+    Random random = new Random();
+
+    for (int i = 0; i < number; i++)
+    {
+        double randomValue = random.Next(-100, 100);
+        array[i] = randomValue;
+    }
+    return array;
+}
+
+void PrintArray(double[] array) // вывод массива
+{
+    for (int i = 0; i < number; i++)
+    {
+        Console.Write($"элемент {i} - ");
+        Console.WriteLine(array[i]);
+    }
+}
+
+int OddIndexCalc(int[] array) // подсчет суммы чисел на нечетных позициях
+{
+    int summ = 0;
+        for (int i = 0; i < number; i++)
+    {
+        if (i%2 != 0)
+        {
+            summ += array[i];
+        }
+    }
+    return summ;
+}
+
+
